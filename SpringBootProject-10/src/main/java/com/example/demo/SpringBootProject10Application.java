bf2397b9ea8b3491daa11c3d35fdc4fa6c878a28
Aniket2503200 <93081630+Aniket2503200@users.noreply.github.com>
2024-08-14 17:23:08 +0530

    Add files via upload

---
+++ SpringBootProject-10/src/main/java/com/example/demo/SpringBootProject10Application.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProject10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProject10Application.class, args);
+	}
+
+}
+//http://localhost:8080/actuator/health
+//http://localhost:8080/actuator/metrics
