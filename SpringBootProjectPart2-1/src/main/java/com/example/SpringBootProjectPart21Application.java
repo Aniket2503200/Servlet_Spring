8673e2d343525399a815f26240cfa370f0d5bb26
Aniket2503200 <93081630+Aniket2503200@users.noreply.github.com>
2024-08-14 17:24:08 +0530

    Add files via upload

---
+++ SpringBootProjectPart2-1/src/main/java/com/example/SpringBootProjectPart21Application.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectPart21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProjectPart21Application.class, args);
+	}
+
+}
+
+//http://localhost:8080/hello
